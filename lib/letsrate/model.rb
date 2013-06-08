4b5d17887cbe5fdc043a81b4f90f031f0eb11216
rohitr <rrrazdan@gmail.com>
2013-06-09 02:41:57 +0530

    Your vote settings

--- lib/letsrate/model.rb
+++ lib/letsrate/model.rb
@@ -1,20 +1,21 @@
 require 'active_support/concern'
 module Letsrate
   extend ActiveSupport::Concern
-  
+
   def rate(stars, user_id, dimension=nil)
     if can_rate? user_id, dimension
       rates(dimension).build do |r|
         r.stars = stars
         r.rater_id = user_id
-        r.save!          
-      end      
+        r.save!
+      end
     else
+      existing_score = rate_object(user_id,dimension).stars
       rate_object(user_id,dimension).update_attributes(:stars =>stars,:rater_id=>user_id)
     end
-    update_rate_average_existing(rate_object(user_id,dimension).stars,stars, dimension)
+    update_rate_average_existing(existing_score ,stars, dimension)
   end
-  
+
   def update_rate_average(stars, dimension=nil)
     if average(dimension).nil?
       RatingCache.create do |avg|
@@ -24,13 +25,13 @@ module Letsrate
         avg.qty = 1
         avg.dimension = dimension
         avg.save!
-      end                     
+      end
     else
       a = average(dimension)
       a.avg = (a.avg*a.qty + stars) / (a.qty+1)
       a.qty = a.qty + 1
       a.save!
-    end   
+    end
   end
 
   def update_rate_average_existing(existing, stars, dimension=nil)
@@ -46,7 +47,6 @@ module Letsrate
     else
       a = average(dimension)
       a.avg = (a.avg*a.qty + stars-existing) / (a.qty)
-      a.qty = a.qty + 1
       a.save!
     end
   end
@@ -56,18 +56,18 @@ module Letsrate
       self.send "rate_average_without_dimension"
     else
       self.send "#{dimension}_average"
-    end      
-  end        
-      
+    end
+  end
+
   def can_rate?(user_id, dimension=nil)
     val = self.connection.select_value("select count(*) as cnt from rates where rateable_id=#{self.id} and rateable_type='#{self.class.name}' and rater_id=#{user_id} and dimension='#{dimension}'").to_i
     if val == 0
       true
     else
       false
-    end         
-  end    
-  
+    end
+  end
+
   def rates(dimension=nil)
     if dimension.nil?
       self.send "rates_without_dimension"
@@ -75,44 +75,44 @@ module Letsrate
       self.send "#{dimension}_rates"
     end
   end
-  
+
   def raters(dimension=nil)
     if dimension.nil?
       self.send "raters_without_dimension"
     else
       self.send "#{dimension}_raters"
-    end      
+    end
   end
 
   module ClassMethods
-    
+
     def letsrate_rater
-      has_many :ratings_given, :class_name => "Rate", :foreign_key => :rater_id       
-    end    
-    
+      has_many :ratings_given, :class_name => "Rate", :foreign_key => :rater_id
+    end
+
     def letsrate_rateable(*dimensions)
       has_many :rates_without_dimension, :as => :rateable, :class_name => "Rate", :dependent => :destroy, :conditions => {:dimension => nil}
-      has_many :raters_without_dimension, :through => :rates_without_dimension, :source => :rater  
-      
-      has_one :rate_average_without_dimension, :as => :cacheable, :class_name => "RatingCache", 
-              :dependent => :destroy, :conditions => {:dimension => nil}
-      
+      has_many :raters_without_dimension, :through => :rates_without_dimension, :source => :rater
 
-      dimensions.each do |dimension|        
-        has_many "#{dimension}_rates", :dependent => :destroy, 
-                                       :conditions => {:dimension => dimension.to_s}, 
-                                       :class_name => "Rate", 
-                                       :as => :rateable
-                                       
-        has_many "#{dimension}_raters", :through => "#{dimension}_rates", :source => :rater         
-        
-        has_one "#{dimension}_average", :as => :cacheable, :class_name => "RatingCache", 
-                                        :dependent => :destroy, :conditions => {:dimension => dimension.to_s}
-      end                                                    
+      has_one :rate_average_without_dimension, :as => :cacheable, :class_name => "RatingCache",
+              :dependent => :destroy, :conditions => {:dimension => nil}
+
+
+      dimensions.each do |dimension|
+        has_many "#{dimension}_rates", :dependent => :destroy,
+                 :conditions => {:dimension => dimension.to_s},
+                 :class_name => "Rate",
+                 :as => :rateable
+
+        has_many "#{dimension}_raters", :through => "#{dimension}_rates", :source => :rater
+
+        has_one "#{dimension}_average", :as => :cacheable, :class_name => "RatingCache",
+                :dependent => :destroy, :conditions => {:dimension => dimension.to_s}
+      end
     end
   end
-    
-end        
+
+end
 
 class ActiveRecord::Base
   include Letsrate
